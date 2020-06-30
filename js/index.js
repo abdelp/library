c61a038eeea2afc9f0fbcdb3cda1cf5b280b47ae
Abdel <juniorperezpy@gmail.com>
2020-06-30 13:46:24 -0400

    Add removeBookLibrary method

--- js/index.js
+++ js/index.js
@@ -12,7 +12,6 @@ function addBookToLibrary() {
   const title = document.getElementById("book-title").value;
   const noPages = document.getElementById("book-no-pages").value;
   const read = document.getElementById("book-read").value;
-  console.log(read);
 
   const book = new Book(author, title, noPages, read);
 
@@ -21,24 +20,40 @@ function addBookToLibrary() {
   render();
 }
 
+function removeBookFromLibrary(idx) {
+  const removeBook = confirm("Are you sure?");
+
+  if(removeBook) {
+    const firstPart = myLibrary.slice(0, idx);
+    const secondPart = myLibrary.slice(idx + 1, myLibrary.length);
+
+    myLibrary = firstPart.concat(secondPart);
+
+    render();
+  }
+}
+
 function cleanForm() {
   document.getElementById("book-author").value = "";
   document.getElementById("book-title").value = "";
   document.getElementById("book-no-pages").value = "";
-  document.getElementById("book-read").value = "off";
+  document.getElementById("book-read").value = "";
 }
 
 function render() {
   let booksContainer = document.getElementById("books-container");
   booksContainer.innerHTML = "";
-  myLibrary.forEach(book => {
+  myLibrary.forEach((book, idx) => {
     let card = document.createElement('div');
     const read = book.read == "on" ? "Yes" : "No";
 
+    card.id = `book-${idx}`;
     card.className = "card mt-3 mb-3";
     card.innerHTML = `<div class='card-body'><h5 class='card-title'>${book.title}</h5> <p class="card-text">Written by: ${book.author}</p>
     <p class="card-text">Pages: ${book.noPages}</p>
-    <p class="card-text">Read: ${read}</p></div>`;
+    <p class="card-text">Read: ${read}</p>
+    <div class="btn btn-danger" onclick="removeBookFromLibrary(${idx})" data-confirm="Are you sure?">Remove</div>
+    </div>`;
     booksContainer.appendChild(card);
   });
 }
